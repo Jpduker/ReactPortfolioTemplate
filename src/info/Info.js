358a092ed426289930f456069e378bf9a1ed06df
Jpduker <jayaprakashnis619@gmail.com>
2023-02-03 21:40:31 +0530

    modified info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -112,8 +112,8 @@ export const info = {
     portfolio: [ // This is where your portfolio projects will be detailed
         {
             title: "Tinder CLone",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+             //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/Jpduker", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
